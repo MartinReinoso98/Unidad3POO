8ee6e9080d25ba122e60a8156752359258341041
Martin Reinoso <111535334+MartinReinoso98@users.noreply.github.com>
2025-05-29 14:00:36 -0300

    Add files via upload

---
+++ 3/ManejadorEmpleado.py
@@ -0,0 +1,37 @@
+import csv
+from Empleado import Empleado
+
+class ManejadorEmpleado:
+    __ListaEmpleados: list
+
+    def __init__(self):
+        self.__ListaEmpleados = []
+
+    def cargarEmpleado(self):
+        with open("empleados.csv", "r") as archivo:
+            reader = csv.reader(archivo, delimiter=";")
+        next(reader)
+        for fila in reader:
+                NombreYApellido=fila[0]
+                IdEmp=int(fila[1])
+                PuestoEmp=fila[2]
+                unEmpleado = Empleado(NombreYApellido,IdEmp,PuestoEmp)
+                self.__ListaEmpleados.append(unEmpleado)
+        archivo.close()
+        
+    def buscarEmpleado(self,nombre):
+        i=0
+        bandera=False
+        objetoEmpleado=None
+        while i < len(self.__ListaEmpleados) and bandera == False:
+            if nombre == self.__ListaEmpleados[i].getNombreYAp():
+                bandera=True
+            else:
+                i+=1
+                
+        if bandera == True:
+            objetoEmpleado=self.__ListaEmpleados[i]
+        
+        return objetoEmpleado
+                
+        
